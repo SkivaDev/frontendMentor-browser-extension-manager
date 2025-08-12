ca72f6c5168821f37648d35fc24a7b89f6f3b5c9
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2025-08-12 09:32:13 -0500

    feat(project): :sparkles: add Header component

---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+            <div>
+                <Image src="" alt="" />
+                <span>i</span>
+                <h1>Extensions</h1>
+            </div>
+            <button>
+                <Image src="" alt="" />
+                <span>i</span>
+            </button>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
